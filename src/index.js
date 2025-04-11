8b2052c15494123da983c7549c6c49c058390b0f
sarthakdev14 <aries.sarthakgupta@gmail.com>
2025-04-12 04:42:18 +0530

    api respose and error handlng

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
 
 dotenv.config({
   path : '.\env'
 })
+
+
 connectDB()
+.then(()=>{
+  app.listen(process.env.port || 8000, ()=>{
+    console.log(`server is running on port ${process.env.port}`)
+  })
+})
+.catch((err)=>{
+  console.log("mongoDB connection falied",err)
+})
